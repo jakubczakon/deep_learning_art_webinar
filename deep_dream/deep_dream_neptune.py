b3ea802c8c6fb120006a8e4c2226d073168a8936
Jakub Czakon <jakub.czakon@ml100.intra.codilime.com>
2017-01-23 19:11:44 +0100

    cleaning some more

--- deep_dream/deep_dream_neptune.py
+++ deep_dream/deep_dream_neptune.py
@@ -79,16 +79,6 @@ img_recognition_network = VGG16(input_tensor=input_template,
 
 layer_dict = utils.get_layer_dict(img_recognition_network)
 
-x = layer_dict[LAYER_NR].output[:,:,:,FILTER_NR]
-shape = layer_dict[LAYER_NR].output_shape[:3]
-
-loss =  K.variable(0.) - COEFF * K.sum(K.square(x))
-
-grads = K.gradients(loss, input_template)
-
-f_outputs = K.function([input_template], [loss] + grads)
-
-
 def neptune_image(raw_image,description):
     stylish_image = Image.fromarray(raw_image)
     return neptune.Image(
@@ -96,22 +86,50 @@ def neptune_image(raw_image,description):
         description=description,
         data=stylish_image)
 
-def eval_loss_and_grads(x):
-    x = x.reshape((1,) + img_size)
-    outs = f_outputs([x])
+BASE_IMAGE_PATH = (ctx.params.base_file)
+DREAM_ITER = (ctx.params.dream_iter)
+LAYER_NR = (ctx.params.layer_nr)
+FILTER_NR = (ctx.params.filter_nr)
+COEFF = (ctx.params.coeff)
+JITTER = (ctx.params.jitter)
+
+def eval_loss_and_grads(img_tensor,layer_dict,
+                       layer_type,layer_nr,filter_nr,coeff):
+ 
+    x = layer_dict[layer_nr].output[:,:,:,filter_nr]
+
+    loss =  - coeff * K.sum(K.square(x))
+
+    grads = K.gradients(loss, input_template)
+
+    grad_loss_func = K.function([input_template], [loss] + grads)
+    
+    img_tensor = img_tensor.reshape((1,)+img_size)
+    
+    outs = grad_loss_func([img_tensor])
+
     loss_value = outs[0]
     grad_values = outs[1].flatten().astype('float64')
 
     return loss_value, grad_values
 
+
 class Evaluator(object):
-    def __init__(self):
+    def __init__(self,layer_dict,layer_nr,filter_nr,coeff):
         self.loss_value = None
         self.grad_values = None
+        self.layer_dict = layer_dict
+        self.layer_nr = layer_nr
+        self.filter_nr = filter_nr
+        self.coeff = coeff
 
     def loss(self, x):
         assert self.loss_value is None
-        loss_value, grad_values = eval_loss_and_grads(x)
+        loss_value, grad_values = eval_loss_and_grads(x,
+                                                      self.layer_dict,
+                                                      self.layer_nr,
+                                                      self.filter_nr,
+                                                      self.coeff)
         self.loss_value = loss_value
         self.grad_values = grad_values
         return self.loss_value
@@ -126,12 +144,15 @@ class Evaluator(object):
 
 if __name__ == "__main__":
 
-    evaluator = Evaluator()
-
     img_dream = img_utils.load_img(BASE_IMAGE_PATH,target_size = img_size[:2])
     x = utils.img2vggtensor(img_dream) 
 
     for i in range(DREAM_ITER):
+        
+        evaluator = Evaluator(layer_dict = layer_dict,
+                              layer_nr = layer_nr,filter_nr = filter_nr,
+                              coeff = coeff)
+        
         logging_channel.send(x = time.time(),y = "iteration %s start"%i)
 
         random_jitter = (JITTER * 2) * (np.random.random(img_size) - 0.5)
@@ -142,7 +163,7 @@ if __name__ == "__main__":
         loss_channel.send(x = i, y = float(min_val))
         logging_channel.send(x = time.time(),y = "iteration %s error %s"%(i,min_val))
 
-        x = x.reshape(img_size)
+        x = x.reshape((1,)+img_size)
         x -= random_jitter
         img = utils.deprocess_image(np.copy(x))
         
